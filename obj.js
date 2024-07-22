0fdb5cc66321de7432f803d9c3776c56fc8fea2e
Rupesh Sahu <rupeshsahu969@gmail.com>
2024-07-22 15:04:07 +0530

    add this dublicate

---
+++ obj.js
@@ -0,0 +1,11 @@
+
+let obj= {
+    name: "Raja",
+    baja:"saja",
+    number:"123456",
+    geetin: function(){
+        console.log(this)
+    }
+    }
+    
+    obj.geetin()
---
+++ objectDublicate.js
@@ -0,0 +1,21 @@
+let arr = [10, 20, 50, 40, 40, 50];
+
+function Data(arr) {
+  let count = " ";
+
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] == undefined) {
+      obj[arr[i]] = 1;
+    } else {
+      obj[arr[i]]++;
+    }
+  }
+  for (let key in obj) {
+    if (obj[key] > 1) count += key + " ";
+  }
+  console.log(count);
+}
+
+Data(arr);

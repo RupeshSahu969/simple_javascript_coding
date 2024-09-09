41a71e021b5309d5c75f258791256ead854d8fa1
Rupesh Sahu <rupeshsahu969@gmail.com>
2024-09-09 21:04:18 +0530

    add this new data

--- vowels.js
+++ vowels.js
@@ -3,15 +3,15 @@ function countVowels(str) {
     const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
     for (let i = 0; i < str.length; i++) {
-        // Manually check if the character matches any vowel
+        
         for (let j = 0; j < vowels.length; j++) {
             if (str[i] === vowels[j]) {
                 count++;
-                break; // Stop checking other vowels once a match is found
+                break; 
             }
         }
     }
     return count;
 }
 
-console.log(countVowels("i")); // Output will be 1
+console.log(countVowels("i")); 

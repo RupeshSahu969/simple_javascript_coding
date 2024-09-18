73b43aa5b26607ec04bfa561b2823da8f3cb84c3
Rupesh Sahu <rupeshsahu969@gmail.com>
2024-09-18 08:39:37 +0530

    chnage this new data

--- pallendrom.js
+++ pallendrom.js
@@ -1,23 +1,17 @@
+let p = "asddsa";
 
-let p="asddsa";
+function Paildrom(p) {
+  let bag = "";
 
-function Paildrom(p){
+  for (let i = p.length - 1; i >= 0; i--) {
+    bag += p[i];
+  }
 
-let bag="";
-
-for(let i=p.length-1; i>=0; i--)
-{
-    bag +=p[i]
+  if (p == bag) {
+    console.log("paildrom");
+  } else {
+    console.log("not paildrom");
+  }
 }
 
-if(p==bag){
-    console.log("paildrom")
-}
-else{
-    console.log("not paildrom")
-}
-
-}
-
-Paildrom(p)
-
+Paildrom(p);
--- reverse.js
+++ reverse.js
@@ -2,14 +2,10 @@ let str = "Hello World";
 
 let rev = "";
 
-function String(str) 
-{
-  for (let i = str.length - 1; i >= 0; i--)
-     {
+function String(str) {
+  for (let i = str.length - 1; i >= 0; i--) {
     rev += str[i];
   }
   console.log(rev);
 }
 String(str);
-
-
